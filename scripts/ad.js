5deb7440a6d875d0ed78e30817d207eb78e6b7c5
Windy Choi <swaf.korea@gmail.com>
2024-02-24 15:26:29 +0900

    코드 변경 내용: ad.html, index.html, ad.js, styles.css 코드 변경 내용: QR 코드에 사용자 ID 추가

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,8 +1,19 @@
-function openQr(type) {
+function openQr(type, userId) {
 	Swal.fire({
 		imageUrl: `images/qr-${type}.jpg`,
 		imageWidth: 360,
 		imageHeight: 360,
 		imageAlt: 'Custom image',
+		backdrop: false,
+		html: `<div class="qr-id">${userId}</div>`,
+		customClass: {
+			popup: 'qr-popup',
+			image: 'qr-img',
+			htmlContainer: 'qr-content',
+		},
+    showCloseButton: true,
+    showConfirmButton: false,
+    focusConfirm: false,
+    inputAutoFocus: false,
 	});
 }
