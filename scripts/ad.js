83c9a89dc4234c65a4a53b0c6a854d901db2d9f9
Windy Choi <swaf.korea@gmail.com>
2024-03-06 19:46:44 +0900

    이미지 및 QR 코드 파일 삭제 및 수정

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,19 +1,19 @@
 function openQr(type, userId) {
 	Swal.fire({
-		imageUrl: `images/qr-${type}.jpg`,
+		imageUrl: `images/qr-codes/${type}/${userId}.jpg`,
 		imageWidth: 360,
 		imageHeight: 360,
 		imageAlt: 'Custom image',
 		backdrop: false,
-		html: `<div class="qr-id">${userId}</div>`,
+		html: `<div class="qr-id">@${userId}</div>`,
 		customClass: {
 			popup: 'qr-popup',
 			image: 'qr-img',
 			htmlContainer: 'qr-content',
 		},
-    showCloseButton: true,
-    showConfirmButton: false,
-    focusConfirm: false,
-    inputAutoFocus: false,
+		showCloseButton: true,
+		showConfirmButton: false,
+		focusConfirm: false,
+		inputAutoFocus: false,
 	});
 }
