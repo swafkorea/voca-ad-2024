c64a81c0a0ecae53fe1b73ea08a58b441b4b3c1f
Windy Choi <swaf.korea@gmail.com>
2024-03-06 19:53:34 +0900

    코드 변경 내용: ad.html 및 index.html에서 WeChat 및 Telegram QR 코드를 클릭할 때 사용자 ID를 전달하지 않도록 변경

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,4 +1,17 @@
+const userIds = {
+	wechat: 'kakawan1234',
+	telegram: 'kakawan1234',
+};
+
 function openQr(type, userId) {
+	userId = userId || userIds[type];
+
+	if (type == 'telegram') {
+		const url = `https://t.me/${userId}`;
+		window.open(url, '_blank');
+		return;
+	}
+
 	Swal.fire({
 		imageUrl: `images/qr-codes/${type}/${userId}.jpg`,
 		imageWidth: 360,
